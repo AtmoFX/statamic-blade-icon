62a0318019b402c97af5093e5e51221ad307cc53
AtmoFX <fxavierdavid@gmail.com>
2025-06-15 14:33:10 +0800

    Created layout for blade page

--- resources/views/blade-icons.blade.php
+++ resources/views/blade-icons.blade.php
@@ -1,7 +1,10 @@
+@extends('blade-layout')
 
+@section('body')
 <div class="grid grid-cols-2 gap-4 p-1">
 @foreach ($icons as $set)
-    <s:svg class="w-10 h-10 hover:text-red" src="{{ $set->augmentedValue('icon')->raw() }}"/>
     {!! $set->icon !!}
+    <s:svg class="w-10 h-10 hover:text-red-500 duration-200" src="{{ $set->raw('icon') }}"/>
 @endforeach
 </div>
+@endsection
